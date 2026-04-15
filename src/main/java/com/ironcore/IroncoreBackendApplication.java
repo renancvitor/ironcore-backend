195f0482614697fe5abb80fb818025e7a7f5247e
Renan C. Vitor <renan.vitor.cm@gmail.com>
2026-04-14 21:52:55 -0300

    chore: initial spring boot project setup

---
+++ src/main/java/com/ironcore/IroncoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ironcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IroncoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IroncoreBackendApplication.class, args);
+	}
+
+}
