6c3d2a9af79981022ce76470b34405b732e2e71a
Renan C. Vitor <renan.vitor.cm@gmail.com>
2026-05-22 23:42:12 -0300

    refactor(auth): refine code base using getter annotation

--- src/main/java/com/ironcore/infrastructure/security/jwt/JwtTokenProperties.java
+++ src/main/java/com/ironcore/infrastructure/security/jwt/JwtTokenProperties.java
@@ -1,37 +1,18 @@
 package com.ironcore.infrastructure.security.jwt;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+@Setter
+@Getter
 @Component
 @ConfigurationProperties(prefix = "security.token")
 public class JwtTokenProperties {
 
     private String secret;
     private String issuer = "IronCore";
-    private Long expirationMinutes = 60L;
+    private Long expirationMinutes = 120L;
 
-    public String getSecret() {
-        return secret;
-    }
-
-    public void setSecret(String secret) {
-        this.secret = secret;
-    }
-
-    public String getIssuer() {
-        return issuer;
-    }
-
-    public void setIssuer(String issuer) {
-        this.issuer = issuer;
-    }
-
-    public Long getExpirationMinutes() {
-        return expirationMinutes;
-    }
-
-    public void setExpirationMinutes(Long expirationMinutes) {
-        this.expirationMinutes = expirationMinutes;
-    }
 }
