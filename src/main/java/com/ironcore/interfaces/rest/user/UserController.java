e5431eda414e2437f5c34d7dc776ca9138ff0cd5
Renan C. Vitor <renan.vitor.cm@gmail.com>
2026-05-24 14:22:15 -0300

    feat(user): add endpoint do initial change password

--- src/main/java/com/ironcore/interfaces/rest/user/UserController.java
+++ src/main/java/com/ironcore/interfaces/rest/user/UserController.java
@@ -2,9 +2,12 @@ package com.ironcore.interfaces.rest.user;
 
 import com.ironcore.application.user.usecase.ChangePasswordCommand;
 import com.ironcore.application.user.usecase.ChangePasswordUseCase;
+import com.ironcore.application.user.usecase.InitialChangePasswordResult;
+import com.ironcore.application.user.usecase.InitialChangePasswordUseCase;
 import com.ironcore.infrastructure.security.auth.AuthenticatedUser;
 import com.ironcore.interfaces.rest.user.dto.ChangePasswordRequest;
-import com.ironcore.interfaces.rest.user.mapper.ChangePasswordMapper;
+import com.ironcore.interfaces.rest.user.dto.InitialChangePasswordResponse;
+import com.ironcore.interfaces.rest.user.mapper.UserChangePasswordMapper;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,15 +20,27 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private final ChangePasswordUseCase changePasswordUseCase;
+    private final InitialChangePasswordUseCase initialChangePasswordUseCase;
 
     @PostMapping("/me/change-password")
     public ResponseEntity<Void> changePassword(
             @AuthenticationPrincipal AuthenticatedUser authenticatedUser,
             @RequestBody @Valid ChangePasswordRequest request
     ) {
-        ChangePasswordCommand command = ChangePasswordMapper.toCommand(authenticatedUser, request);
+        ChangePasswordCommand command = UserChangePasswordMapper.toChangePasswordCommand(authenticatedUser, request);
         changePasswordUseCase.execute(command);
 
         return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/me/change-initial-password")
+    public ResponseEntity<InitialChangePasswordResponse>  changeInitialPassword(
+            @AuthenticationPrincipal AuthenticatedUser authenticatedUser,
+            @RequestBody @Valid ChangePasswordRequest request
+    ) {
+        ChangePasswordCommand command = UserChangePasswordMapper.toChangePasswordCommand(authenticatedUser, request);
+        InitialChangePasswordResult result = initialChangePasswordUseCase.execute(command);
+
+        return ResponseEntity.ok(UserChangePasswordMapper.toInitialChangePasswordResponse(result));
     }
 }
