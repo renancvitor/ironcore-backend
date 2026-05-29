506aeb456d1306b6d86a42fdcccc8d92e25ee1e0
Renan C. Vitor <98927761+renancvitor@users.noreply.github.com>
2026-05-28 21:08:50 -0300

    feat(exception): add specific exceptions to improve clarity (#85)
    * feat(exception): add specific exceptions to clarity
    
    * test(auth): add web tests to errors 403, 401 and 500
    
    * refactor(user): improve clarity with specific inactive user exception
    
    * refactor(user): add correctly annotation and clear error message

--- src/main/java/com/ironcore/interfaces/rest/user/dto/InitialChangePasswordRequest.java
+++ src/main/java/com/ironcore/interfaces/rest/user/dto/InitialChangePasswordRequest.java
@@ -1,8 +1,10 @@
 package com.ironcore.interfaces.rest.user.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record InitialChangePasswordRequest(
+        @Email(message = "O e-mail informado possui um formato inválido.")
         @NotBlank(message = "O e-mail não pode estar em branco.")
         String email,
 
