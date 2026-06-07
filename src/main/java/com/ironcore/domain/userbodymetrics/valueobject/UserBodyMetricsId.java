bae4bb21a7a995af18c28e94c72259a90a569d8d
Renan C. Vitor <renan.vitor.cm@gmail.com>
2026-06-07 07:43:22 -0300

    feat(userbodymetrics): add specific exception
    - This exception is not generic; it is compatible with current usage

--- src/main/java/com/ironcore/domain/userbodymetrics/valueobject/UserBodyMetricsId.java
+++ src/main/java/com/ironcore/domain/userbodymetrics/valueobject/UserBodyMetricsId.java
@@ -1,12 +1,12 @@
 package com.ironcore.domain.userbodymetrics.valueobject;
 
-import com.ironcore.domain.userbodymetrics.exception.InvalidBodyMetricException;
+import com.ironcore.domain.userbodymetrics.exception.InvalidUserBodyMetricsException;
 
 public record UserBodyMetricsId(Long value) {
 
     public UserBodyMetricsId {
         if (value == null || value <= 0) {
-            throw new InvalidBodyMetricException("Id das métricas corporais do usuário deve ser positivo.");
+            throw new InvalidUserBodyMetricsException("Id das métricas corporais do usuário deve ser positivo.");
         }
     }
 }
