a2dc2301efd48fa490d1130085233acc57e69937
Renan C. Vitor <renan.vitor.cm@gmail.com>
2026-06-07 12:47:37 -0300

    test(userbodymetrics): add unit tests to value objects

---
+++ src/test/java/com/ironcore/domain/userbodymetrics/valueobject/BodyCircumferenceCmTest.java
@@ -0,0 +1,47 @@
+package com.ironcore.domain.userbodymetrics.valueobject;
+
+import com.ironcore.domain.userbodymetrics.exception.InvalidBodyMetricException;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class BodyCircumferenceCmTest {
+
+    @Test
+    void shouldReturnExpectedBodyCircumferenceCm() {
+        BodyCircumferenceCm bodyCircumferenceCm = new BodyCircumferenceCm(90.0);
+
+        assertThat(bodyCircumferenceCm.value()).isEqualTo(90.0);
+    }
+
+    @Test
+    void shouldFailWhenBodyCircumferenceCmIsNull() {
+        assertThatThrownBy(() -> new BodyCircumferenceCm(null))
+                .isInstanceOf(InvalidBodyMetricException.class);
+    }
+
+    @Test
+    void shouldFailWhenBodyCircumferenceCmIsNotFinite() {
+        assertThatThrownBy(() -> new BodyCircumferenceCm(Double.POSITIVE_INFINITY))
+                .isInstanceOf(InvalidBodyMetricException.class);
+    }
+
+    @Test
+    void shouldFailWhenBodyCircumferenceCmIsZero() {
+        assertThatThrownBy(() -> new BodyCircumferenceCm(0.0))
+                .isInstanceOf(InvalidBodyMetricException.class);
+    }
+
+    @Test
+    void shouldFailWhenBodyCircumferenceCmIsNegative() {
+        assertThatThrownBy(() -> new BodyCircumferenceCm(-1.0))
+                .isInstanceOf(InvalidBodyMetricException.class);
+    }
+
+    @Test
+    void shouldFailWhenBodyCircumferenceCmIsGreaterThanMaximumAllowed() {
+        assertThatThrownBy(() -> new BodyCircumferenceCm(300.1))
+                .isInstanceOf(InvalidBodyMetricException.class);
+    }
+}
---
+++ src/test/java/com/ironcore/domain/userbodymetrics/valueobject/LeanMassKgTest.java
@@ -0,0 +1,43 @@
+package com.ironcore.domain.userbodymetrics.valueobject;
+
+import com.ironcore.domain.userbodymetrics.exception.InvalidBodyMetricException;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class LeanMassKgTest {
+
+    private static final double MAX_EXPECTED_WEIGHT_KG = 500.0;
+
+    @Test
+    void shouldReturnExpectedLeanMassKg() {
+        LeanMassKg correctlyWeight = new LeanMassKg(70.0);
+
+        assertThat(correctlyWeight.value()).isLessThan(MAX_EXPECTED_WEIGHT_KG);
+    }
+
+    @Test
+    void shouldFailWhenLeanMassKgIsNull() {
+        assertThatThrownBy(() -> new LeanMassKg(null))
+                .isInstanceOf(InvalidBodyMetricException.class);
+    }
+
+    @Test
+    void shouldFailWhenLeanMassKgIsNotFinite() {
+        assertThatThrownBy(() -> new LeanMassKg(Double.POSITIVE_INFINITY))
+                .isInstanceOf(InvalidBodyMetricException.class);
+    }
+
+    @Test
+    void shouldFailWhenLeanMassKgIsNotPositive() {
+        assertThatThrownBy(() -> new LeanMassKg(-100.0))
+                .isInstanceOf(InvalidBodyMetricException.class);
+    }
+
+    @Test
+    void shouldFailWhenLeanMassKgIsMajorThanMaxExpectedWeightKg() {
+        assertThatThrownBy(() -> new LeanMassKg(500.1))
+                .isInstanceOf(InvalidBodyMetricException.class);
+    }
+}
