f215591f2232fd634b9a7f01fb88a8fc9c9f7bb5
Renan C. Vitor <renan.vitor.cm@gmail.com>
2026-07-07 11:22:23 -0300

    feat(swagger): add schema documentations for error response

---
+++ src/main/java/com/ironcore/interfaces/rest/openapi/NotFoundResponse.java
@@ -0,0 +1,26 @@
+package com.ironcore.interfaces.rest.openapi;
+
+import com.ironcore.interfaces.rest.exception.model.ApiErrorResponse;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.http.MediaType;
+
+import java.lang.annotation.*;
+
+@Documented
+@Target({
+        ElementType.METHOD,
+        ElementType.ANNOTATION_TYPE
+})
+@Retention(RetentionPolicy.RUNTIME)
+@ApiResponse(
+        responseCode = "404",
+        description = "Recurso não encontrado",
+        content = @Content(
+                mediaType = MediaType.APPLICATION_JSON_VALUE,
+                schema = @Schema(implementation = ApiErrorResponse.class)
+        )
+)
+public @interface NotFoundResponse {
+}
