60bc2629ea90035c06b98f8bef1c441a873c7fbf
Renan C. Vitor <renan.vitor.cm@gmail.com>
2026-07-11 18:43:00 -0300

    feat(musclegroup): add domain layer implementation

---
+++ src/main/java/com/ironcore/domain/muscle/musclegroup/exception/InvalidMuscleGroupException.java
@@ -0,0 +1,10 @@
+package com.ironcore.domain.muscle.musclegroup.exception;
+
+import com.ironcore.domain.exception.DomainException;
+
+public class InvalidMuscleGroupException extends DomainException {
+
+    public InvalidMuscleGroupException(String message) {
+        super(message);
+    }
+}
