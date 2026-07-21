969c65575079dc948a38cb1e878a586fb501872b
Renan C. Vitor <98927761+renancvitor@users.noreply.github.com>
2026-07-21 20:29:44 -0300

    refactor(exercise): remove unuseful contracts methods

--- src/main/java/com/ironcore/domain/exercise/repository/ExerciseRepository.java
+++ src/main/java/com/ironcore/domain/exercise/repository/ExerciseRepository.java
@@ -1,20 +1,11 @@
 package com.ironcore.domain.exercise.repository;
 
-import com.ironcore.domain.activitytype.valueobject.ActivityTypeId;
-import com.ironcore.domain.equipmenttype.valueobject.EquipmentTypeId;
 import com.ironcore.domain.exercise.model.Exercise;
 import com.ironcore.domain.exercise.valueobject.ExerciseId;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface ExerciseRepository {
 
     Optional<Exercise> findById(ExerciseId id);
-
-    List<Exercise> findByEquipmentTypeId(EquipmentTypeId equipmentTypeId);
-
-    List<Exercise> findByActivityTypeId(ActivityTypeId activityTypeId);
-
-    List<Exercise> findAll();
 }
