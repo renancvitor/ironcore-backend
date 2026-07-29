15c53f819d2a6e0f9e039cd94279b932f5059960
Renan C. Vitor <98927761+renancvitor@users.noreply.github.com>
2026-07-29 20:28:29 -0300

    feat(exercise): add JPA methods to implement query port

--- src/main/java/com/ironcore/infrastructure/persistence/exercisemuscletarget/repository/ExerciseMuscleTargetJpaRepository.java
+++ src/main/java/com/ironcore/infrastructure/persistence/exercisemuscletarget/repository/ExerciseMuscleTargetJpaRepository.java
@@ -3,5 +3,11 @@ package com.ironcore.infrastructure.persistence.exercisemuscletarget.repository;
 import com.ironcore.infrastructure.persistence.exercisemuscletarget.entity.ExerciseMuscleTargetEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ExerciseMuscleTargetJpaRepository extends JpaRepository<ExerciseMuscleTargetEntity, Long> {
+
+    List<ExerciseMuscleTargetEntity> findAllByExercise_IdAndActiveTrueOrderByTargetRoleAscMuscleSubgroup_DisplayNameAsc(
+            Long exerciseId
+    );
 }
