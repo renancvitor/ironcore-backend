666059c969e59361fbc29cee9f783d173aff434c
Renan C. Vitor <renan.vitor.cm@gmail.com>
2026-08-12 20:56:06 -0300

    feat(workoutcycle): add domain repository

---
+++ src/main/java/com/ironcore/domain/workoutplanning/workoutcycle/repository/WorkoutCycleRepository.java
@@ -0,0 +1,23 @@
+package com.ironcore.domain.workoutplanning.workoutcycle.repository;
+
+import com.ironcore.domain.person.valueobject.PersonId;
+import com.ironcore.domain.workoutplanning.traininggoal.valueobject.TrainingGoalId;
+import com.ironcore.domain.workoutplanning.workoutcycle.enums.WorkoutStatus;
+import com.ironcore.domain.workoutplanning.workoutcycle.model.WorkoutCycle;
+import com.ironcore.domain.workoutplanning.workoutcycle.valueobject.WorkoutCycleId;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface WorkoutCycleRepository {
+
+    WorkoutCycle save(WorkoutCycle workoutCycle);
+
+    Optional<WorkoutCycle> findByIdAndPersonId(WorkoutCycleId id, PersonId personId);
+
+    List<WorkoutCycle> findByPersonId(PersonId personId);
+
+    List<WorkoutCycle> findByPersonIdAndWorkoutStatus(PersonId personId, WorkoutStatus workoutStatus);
+
+    List<WorkoutCycle> findByPersonIdAndTrainingGoalId(PersonId personId, TrainingGoalId trainingGoalId);
+}
